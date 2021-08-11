3903c4b419b950140b07ba24c5ecfd963da46db9
Florencia Sosa <florenciassoledadsosa@gmail.com>
2021-08-11 18:27:56 -0300

    ejercicio 2

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,21 @@
-/*
-    ===== Código de TypeScript =====
-*/
+import { calculaISV, Producto } from './desestructuracion-de-argumentos';
 
-interface Superheroe {
-  nombre: string;
-  edad: number;
-  direccion: Direccion;
-  mostrarDireccion: () => string;
-}
-interface Direccion {
-  calle: string;
-  pais: string;
-  ciudad: string;
-}
-const superheroe: Superheroe = {
-  nombre: 'Spiderman',
-  edad: 30,
-  direccion: {
-    calle: 'Main St',
-    pais: 'USA',
-    ciudad: 'NY',
+const carritoCompras: Producto[] = [
+  {
+    descripcion: 'celular',
+    precio: 500,
   },
-  mostrarDireccion() {
-    return (
-      'El ' +
-      this.nombre +
-      ',vive en' +
-      this.direccion.pais +
-      ',' +
-      this.direccion.ciudad
-    );
+  {
+    descripcion: 'telefono',
+    precio: 100,
   },
-};
-/*Desestructuración de objetos*/
-/*Desestructuración de arrays es por posicionamiento*/
-const {nombre:nombreDeHeroe, edad:edadDeHeroe,} = superheroe;
-const {calle, pais, ciudad}= superheroe.direccion
-console.log("El nombre del Heroe es: " + nombreDeHeroe);
-console.log("La edad es: " + edadDeHeroe);
-console.log(nombreDeHeroe + " vive en la calle " + calle + ", en la ciudad de " + ciudad + " en " + pais )
+  {
+    descripcion: 'pc    ',
+    precio: 100,
+  }
+];
+
+const [total, isv] = calculaISV(carritoCompras);
+
+console.log('total:', total)
+console.log('ISV: ', isv)
