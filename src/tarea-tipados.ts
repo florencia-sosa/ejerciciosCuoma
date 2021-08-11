eff65d7f306536a1fcd53dced01668be5f1b7c13
Florencia Sosa <florenciassoledadsosa@gmail.com>
2021-08-11 18:27:27 -0300

    Ejercicio1

--- src/tarea-tipados.ts
+++ src/tarea-tipados.ts
@@ -1,17 +1,19 @@
+/*
+    ===== Código de TypeScript =====
+*/
 
 interface Superheroe {
   nombre: string;
   edad: number;
-  direccion: Direccion; 
-  mostrarDireccion: () =>string;
+  direccion: Direccion;
+  mostrarDireccion: () => string;
 }
- interface Direccion{
-    calle: string;
-    pais: string;
-    ciudad: string;
-
- }
-const superHeroe: Superheroe = {
+interface Direccion {
+  calle: string;
+  pais: string;
+  ciudad: string;
+}
+const superheroe: Superheroe = {
   nombre: 'Spiderman',
   edad: 30,
   direccion: {
@@ -19,10 +21,22 @@ const superHeroe: Superheroe = {
     pais: 'USA',
     ciudad: 'NY',
   },
-  mostrarDireccion(){
-      return "El " + this.nombre + ',vive en' + this.direccion.pais + ',' + this.direccion.ciudad;
+  mostrarDireccion() {
+    return (
+      'El ' +
+      this.nombre +
+      ',vive en' +
+      this.direccion.pais +
+      ',' +
+      this.direccion.ciudad
+    );
   },
 };
+/*Desestructuración de objetos*/
+/*Desestructuración de arrays es por posicionamiento*/
+const {nombre:nombreDeHeroe, edad:edadDeHeroe } = superheroe;
+const {calle, pais, ciudad}= superheroe.direccion
+console.log("El nombre del Heroe es: " + nombreDeHeroe);
+console.log("La edad es: " + edadDeHeroe);
+console.log(nombreDeHeroe + " vive en la calle " + calle + ", en la ciudad de " + ciudad + " en " + pais )
 
-
-console.log(superheroe.mostrarDireccion())
